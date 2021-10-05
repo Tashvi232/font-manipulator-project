c9df2ac42cddc9951d7b40fdb9046e1212494cf5
Tashvi232 <79177478+Tashvi232@users.noreply.github.com>
2021-10-05 16:17:51 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+leftWristX=0;
+rightWristX=0;
+distance=0;
 function setup() {
 video=createCapture(VIDEO);
-size(300,300);
-canvas=createCanvas();
-posenet=ml5.posenet(video,modelLoaded);
-pose.on();
+video.size(550,500);
+canvas=createCanvas(550,550);
+canvas.position(560,150);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
 
 }
 
@@ -12,11 +16,18 @@ function modelLoaded() {
 }
 
 function draw() {
-
+    background(130, 209, 209);
+    textSize(distance);
+    fill(20, 61, 89);
+    text("Tashvi",560,150)
 }
 
 function gotPoses(result) {
     if(result>0) {
         console.log(result);
+        leftWristX=result[0].pose.leftWrist.x;
+    rightWristX=result[0].pose.rightWrist.x;
+    distance=floor(leftWristX-rightWristX);
+    console.log("leftWristX= "+leftWristX+" rightWristX= "+rightWristX+" distance= "+distance);
     }
 }
